b6edae4e7d5157d14f3d43a30e445b89a71202ca
hillarioh <okerioh@gmail.com>
2021-03-04 11:09:29 +0300

    complete category

--- src/components/category.js
+++ src/components/category.js
@@ -1,6 +1,8 @@
 import React from "react";
 import cart from "../assets/images/cart.png";
 import cartItem from "../assets/images/cartItem.png";
+import cartItem2 from "../assets/images/cartitem2.png";
+import cartItem3 from "../assets/images/cartitem3.png";
 
 function Category() {
   return (
@@ -19,10 +21,58 @@ function Category() {
       <div className="red-bg"></div>
       <div className="category">
         <div className="category-item">
+          <div className="category-item-tag"></div>
+          <img
+            src={cartItem}
+            alt="cart item"
+            style={{ height: 130, position: "absolute", right: 5, top: 0 }}
+          />
           <div className="category-item-details">
-            <div>
-              <img src={cartItem} alt="" srcset="" />
+            <span>MARINI Men Waves Pomade</span>
+            <div className="category-item-cart">
+              <h4>KES 3300</h4>
+              <img src={cart} alt="cart icon" style={{}} />
             </div>
+          </div>
+        </div>
+        <div className="category-item">
+          <div className="category-item-tag"></div>
+          <img
+            src={cartItem2}
+            alt="cart item"
+            style={{ height: 130, position: "absolute", right: -10, top: 0 }}
+          />
+          <div className="category-item-details">
+            <span>MARINI Men Waves Pomade</span>
+            <div className="category-item-cart">
+              <h4>KES 3300</h4>
+              <img src={cart} alt="cart icon" style={{}} />
+            </div>
+          </div>
+        </div>
+        <div className="category-item">
+          <div className="category-item-tag"></div>
+          <img
+            src={cartItem3}
+            alt="cart item"
+            style={{ height: 160, position: "absolute", right: -12, top: -5 }}
+          />
+          <div className="category-item-details">
+            <span>MARINI Men Waves Pomade</span>
+            <div className="category-item-cart">
+              <h4>KES 3300</h4>
+              <img src={cart} alt="cart icon" style={{}} />
+            </div>
+          </div>
+        </div>
+        <div className="category-item">
+          <div className="category-item-tag"></div>
+          <img
+            src={cartItem}
+            alt="cart item"
+            style={{ height: 130, position: "absolute", right: 5, top: 0 }}
+          />
+          <div className="category-item-details">
             <span>MARINI Men Waves Pomade</span>
             <div className="category-item-cart">
               <h4>KES 3300</h4>
