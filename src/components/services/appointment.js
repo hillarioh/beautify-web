a1255d03cc83ef1f5db6b61a1be68b0c068172b3
hillarioh <okerioh@gmail.com>
2021-03-10 12:36:17 +0300

    add appointment,dashboard and appoint component

--- src/components/services/appointment.js
+++ src/components/services/appointment.js
@@ -1,9 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ellipse from "../../assets/images/ellipse.png";
+import mail from "../../assets/images/mail.png";
+import Appoint from "./appoint";
 
 function Appointment() {
+  const [currentView, setCurrentView] = useState("1");
+  const [currApp, setCurrApp] = useState({});
+  const [appointment, setAppointment] = useState([
+    {
+      id: "1",
+      user_name: "Patrick",
+      service_name: "Haircut",
+      date: "12/03/2021",
+    },
+    {
+      id: "2",
+      user_name: "Tim",
+      service_name: "Facial",
+      date: "12/03/2021",
+    },
+    {
+      id: "3",
+      user_name: "Maddy",
+      service_name: "Blow dry",
+      date: "12/03/2021",
+    },
+  ]);
+
+  useEffect(() => {
+    const value = appointment.filter((app) => app.id === currentView);
+    setCurrApp(value[0]);
+  });
+
   return (
     <>
-      <div className="appointment"></div>
+      <div className="appointment">
+        <div className="user-details">
+          <h3>Username</h3>
+          <img src={ellipse} alt="ellipse" />
+          <img src={mail} alt="mail" />
+        </div>
+        <div className="appointment-info">
+          <div className="userlist">
+            <div className="appnt-list">
+              {appointment.map((ap) => {
+                return (
+                  <Appoint appoint={ap} key={ap.id} setId={setCurrentView} />
+                );
+              })}
+            </div>
+          </div>
+          <div className="app-info">
+            <div className="info-header">
+              <span></span>
+              <div>
+                <h3>{currApp.service_name || "Haircut"}</h3>
+                <h4>AT CASTORIS BARBERSHOP</h4>
+              </div>
+            </div>
+            <p>
+              You have an appointment today with
+              {` ${currApp.id}, ${currApp.user_name} `}
+              today at <span>{currApp.date}</span>
+            </p>
+            <div>
+              <button>Confirm</button>
+              <button>Cancel & Notify</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/services/dashboard.js
+++ src/components/services/dashboard.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ellipse from "../../assets/images/ellipse.png";
 import mail from "../../assets/images/mail.png";
+import Appoint from "./appoint";
 
 function Dashboard() {
+  const [clients, setClient] = useState(800);
+  const [currentView, setCurrentView] = useState("1");
+  const [total, setTotal] = useState(600800);
+  const [appointment, setAppointment] = useState([
+    {
+      user_name: "Patrick",
+      service_name: "Haircut",
+      date: "12/03/2021",
+    },
+    {
+      user_name: "Tim",
+      service_name: "Facial",
+      date: "12/03/2021",
+    },
+    {
+      user_name: "Maddy",
+      service_name: "Blow dry",
+      date: "12/03/2021",
+    },
+  ]);
   return (
     <>
       <div className="dashboard">
@@ -12,11 +33,29 @@ function Dashboard() {
           <img src={mail} alt="mail" />
         </div>
         <div className="dashboard-info">
-          <div className="clients"></div>
-          <div className="total"></div>
+          <div className="clients">
+            <h2>{clients}</h2>
+            <p>Clients Through the Last Year</p>
+          </div>
+          <div className="total">
+            <h2>{total}</h2>
+            <p>Total Amount Transacted</p>
+          </div>
           <div className="annual"></div>
           <div className="calendar"></div>
-          <div className="appnt"></div>
+          <div className="appnt">
+            <ul>
+              <li>Appointments</li>
+              <li>January 20 2021</li>
+            </ul>
+            <div className="appnt-list">
+              {appointment.map((ap) => {
+                return (
+                  <Appoint appoint={ap} key={ap.id} setId={setCurrentView} />
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </>
