b5cedac7067ac36ae363379ee9d3c85575c49c17
hillarioh <okerioh@gmail.com>
2021-03-12 12:52:54 +0300

    add calendar

--- src/components/services/dashboard.js
+++ src/components/services/dashboard.js
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 import ellipse from "../../assets/images/ellipse.png";
 import mail from "../../assets/images/mail.png";
 import Appoint from "./appoint";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 function Dashboard() {
   const [clients, setClient] = useState(800);
   const [currentView, setCurrentView] = useState("1");
   const [total, setTotal] = useState(600800);
+  const [value, onChange] = useState(new Date());
   const [appointment, setAppointment] = useState([
     {
       user_name: "Patrick",
@@ -42,7 +45,9 @@ function Dashboard() {
             <p>Total Amount Transacted</p>
           </div>
           <div className="annual"></div>
-          <div className="calendar"></div>
+          <div className="calendar">
+            <Calendar onChange={onChange} value={value} />
+          </div>
           <div className="appnt">
             <ul>
               <li>Appointments</li>
